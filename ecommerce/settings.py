b467b91592e83ec0a79f00f2d8f1cb291cbc78dd
kevinpoly659 <112921938+kevinpoly659@users.noreply.github.com>
2023-08-06 01:44:36 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -89,17 +89,22 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecommerce',
+#         'USER': 'kevin',
+#         'PASSWORD': '9895',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecommerce',
-        'USER': 'kevin',
-        'PASSWORD': '9895',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
